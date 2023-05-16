68305826ca47993e63db5b454d5c767ec87af0f1
Michael Fester <michael.fester@gmail.com>
2023-05-16 16:40:03 -0700

    Fix button role

--- components/user/ChatWindow.tsx
+++ components/user/ChatWindow.tsx
@@ -308,7 +308,11 @@ export const MarkpromptPromptWindow = () => {
 
   return (
     <DocsPrompt onOpenChange={setPromptOpen}>
-      <div className="fixed right-8 bottom-8">
+      <div
+        className="fixed right-8 bottom-8"
+        role="region"
+        aria-label="Ask Markprompt"
+      >
         <button
           className="transform rounded-full border border-neutral-800 bg-neutral-900 p-3 outline-none transition duration-300 hover:bg-neutral-1000"
           role="button"
