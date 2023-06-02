1931b5a9a5ad9a2b692e91aea1d1d2c32d1f6779
Michael Fester <michael.fester@gmail.com>
2023-06-02 16:07:29 -0700

    Update sql migration and schema

--- config/schema.sql
+++ config/schema.sql
@@ -204,6 +204,13 @@ begin
 end;
 $$;
 
+create or replace function refresh_materialized_view(view_name text)
+returns void as $$
+begin
+  refresh materialized view view_name;
+end;
+$$ language plpgsql;
+
 -- Indexes
 
 create index idx_files_source_id on files(source_id);
@@ -212,7 +219,21 @@ create index idx_file_sections_file_id on file_sections(file_id);
 create index idx_projects_team_id on projects(team_id);
 create index idx_memberships_user_id on memberships(user_id);
 
--- Materialized views
+-- Views
+
+create view v_team_project_usage_info as
+  select
+    projects.id as project_id,
+    teams.id as team_id,
+    teams.is_enterprise_plan as is_enterprise_plan,
+    teams.stripe_price_id as stripe_price_id,
+    sum(file_sections.token_count) as team_token_count
+  from file_sections
+  left join files on file_sections.file_id = files.id
+  left join sources on files.source_id = sources.id
+  left join projects on sources.project_id = projects.id
+  left join teams on projects.team_id = teams.id
+  group by projects.id, teams.id
 
 -- RLS
 
