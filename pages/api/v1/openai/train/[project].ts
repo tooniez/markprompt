97a2c4dffc726e90ab7249e8e0e3c119c6fe59f5
Michael Fester <michael.fester@gmail.com>
2023-06-03 16:27:37 -0700

    Update train endpoint

--- pages/api/v1/openai/train/[project].ts
+++ pages/api/v1/openai/train/[project].ts
@@ -192,17 +192,21 @@ export default async function handler(
       if (body?.files && Array.isArray(body.files)) {
         // v1
         filesWithPath = body.files
-          .map((f: unknown) => {
-            if (f === null || typeof f !== 'object') return undefined;
-            if (!('id' in f) || !('content' in f)) return undefined;
+          .map((f: any) => {
+            const path = f.path || f.id; // f.id for backwards compatibility
 
-            if (typeof f.id !== 'string' || typeof f.content !== 'string') {
+            if (
+              typeof path !== 'string' ||
+              typeof f.content !== 'string' ||
+              !path ||
+              !f.content
+            ) {
               return undefined;
             }
 
             if (
               !shouldIncludeFileWithPath(
-                f.id,
+                path,
                 markpromptConfig.include || [],
                 markpromptConfig.exclude || [],
                 false,
@@ -212,8 +216,8 @@ export default async function handler(
             }
 
             return {
-              path: f.id,
-              name: getNameFromPath(f.id),
+              path,
+              name: getNameFromPath(path),
               content: f.content,
             };
           })
