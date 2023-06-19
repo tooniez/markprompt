717390178ecdfd563dfc0de34e944d7244b45120
Michael Fester <michael.fester@gmail.com>
2023-06-19 09:15:07 -0700

    Make tooltip clickable

--- components/ui/InfoTooltip.tsx
+++ components/ui/InfoTooltip.tsx
@@ -1,6 +1,7 @@
 import * as Tooltip from '@radix-ui/react-tooltip';
 import cn from 'classnames';
 import { Info } from 'lucide-react';
+import { useState } from 'react';
 
 export const InfoTooltip = ({
   message,
@@ -9,11 +10,17 @@ export const InfoTooltip = ({
   message: string;
   dimmed?: boolean;
 }) => {
+  const [open, setOpen] = useState(false);
   return (
     <Tooltip.Provider>
-      <Tooltip.Root>
+      <Tooltip.Root open={open} onOpenChange={(open) => setOpen(open)}>
         <Tooltip.Trigger asChild>
-          <button className="button-ring flex w-min cursor-default cursor-pointer flex-row items-center gap-2 truncate whitespace-nowrap rounded-md text-xs text-neutral-300">
+          <button
+            className="button-ring flex w-min cursor-pointer flex-row items-center gap-2 truncate whitespace-nowrap rounded-md text-xs text-neutral-300"
+            onClick={() => {
+              setOpen(!open);
+            }}
+          >
             <Info
               className={cn('h-3 w-3', {
                 'text-neutral-300': !dimmed,
