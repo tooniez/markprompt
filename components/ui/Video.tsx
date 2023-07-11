4bb0c316c7570122bf884a933dfc1d8ef08da580
Michael Fester <michael.fester@gmail.com>
2023-07-11 11:20:47 -0700

    Update video tag

--- components/ui/Video.tsx
+++ components/ui/Video.tsx
@@ -8,9 +8,14 @@ type VideoProps = {
 export const Video: FC<VideoProps> = ({ src }) => {
   return (
     <div className={cn('overflow-hidden rounded-md border border-neutral-900')}>
-      <video autoPlay={true} className="w-full" preload="auto">
-        <source src={src} type="video/webm" />
-      </video>
+      <video
+        playsInline
+        loop
+        autoPlay={true}
+        className="w-full"
+        preload="auto"
+        src={src}
+      ></video>
     </div>
   );
 };
