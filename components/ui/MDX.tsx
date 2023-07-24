ed2a6c4a3b150b2e2590ab087a32afef5fb3323d
Marie <schneegansm@gmail.com>
2023-07-24 05:31:12 +0000

    Update legal

--- components/ui/MDX.tsx
+++ components/ui/MDX.tsx
@@ -1,13 +1,28 @@
+import Head from 'next/head';
+import Link from 'next/link';
 import { FC, ReactNode } from 'react';
+
+import { MarkpromptIcon } from '../icons/Markprompt';
 
 type MDXComponentProps = {
   children: ReactNode;
+  meta: any;
 };
 
-export const MDXComponent: FC<MDXComponentProps> = ({ children }) => {
+export const MDXComponent: FC<MDXComponentProps> = ({ children, meta }) => {
   return (
-    <div className="prose prose-invert mx-auto max-w-screen-md p-8">
-      {children}
-    </div>
+    <>
+      <Head>
+        <title>{`${meta.title} | Markprompt`}</title>
+      </Head>
+      <div className="prose prose-invert mx-auto mt-16 max-w-screen-md px-8 pb-20 pt-12">
+        <div className="mb-20">
+          <Link href="/" className="outline-none">
+            <MarkpromptIcon className="h-20 w-20 text-white" />
+          </Link>
+        </div>
+        {children}
+      </div>
+    </>
   );
 };
