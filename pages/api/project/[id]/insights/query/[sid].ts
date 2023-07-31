e8e9481178feac31176dbda74e8a52f7350f4717
Michael Fester <michael.fester@gmail.com>
2023-07-31 10:24:18 -0700

    Fix query stats details

--- pages/api/project/[id]/insights/query/[sid].ts
+++ pages/api/project/[id]/insights/query/[sid].ts
@@ -32,21 +32,32 @@ export default async function handler(
   }
 
   if (req.method === 'GET') {
+    const id = req.query.sid as DbQueryStat['id'];
     const { data, error } = await supabase
       .from('query_stats')
-      .select('id,created_at,prompt,response,no_response,feedback,meta')
-      .match({
-        id: req.query.sid as DbQueryStat['id'],
-        processed_state: 'processed',
-      })
+      .select(
+        'id,created_at,prompt,response,no_response,feedback,meta,processed_state',
+      )
+      .eq('id', id)
       .limit(1)
       .maybeSingle();
 
+    if (
+      !(
+        data?.processed_state === 'processed' ||
+        data?.processed_state === 'skipped'
+      )
+    ) {
+      return res.status(404).json({ error: 'No matching query stat found.' });
+    }
+
     if (error) {
+      console.error('Error fetching query stat:', error.message);
       return res.status(400).json({ error: error.message });
     }
 
     if (!data) {
+      console.error('Query stat not found');
       return res.status(404).json({ error: 'No matching query stat found.' });
     }
 
