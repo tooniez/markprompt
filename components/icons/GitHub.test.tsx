311508db849d17ad828a280285a9d8f10e2acb14
Nick Rutten <2504906+nickrttn@users.noreply.github.com>
2023-08-18 16:34:55 +0200

    Add a type test

--- components/icons/GitHub.test.tsx
+++ components/icons/GitHub.test.tsx
@@ -1,5 +1,5 @@
 import { render } from '@testing-library/react';
-import { describe, expect, it } from 'vitest';
+import { describe, expect, expectTypeOf, it } from 'vitest';
 
 import { GitHubIcon } from './GitHub';
 
@@ -8,5 +8,6 @@ describe('GitHubIcon', () => {
     const { container } = render(<GitHubIcon />);
     const el = container.querySelector('svg');
     expect(el).toBeInTheDocument();
+    expectTypeOf(el).toEqualTypeOf<SVGSVGElement | null>();
   });
 });
