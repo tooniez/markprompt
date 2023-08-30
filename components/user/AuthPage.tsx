6a690ac67135e977f6feb83af11594282ad67c96
Marie <schneegansm@gmail.com>
2023-08-30 17:31:16 +0000

    Add Azure login

--- components/user/AuthPage.tsx
+++ components/user/AuthPage.tsx
@@ -37,7 +37,7 @@ const AuthPage: FC<AuthPageProps> = ({ type }) => {
               redirectTo={getAppOrigin() + '/'}
               onlyThirdPartyProviders
               socialLayout="vertical"
-              providers={['github', 'google']}
+              providers={['github', 'google', 'azure']}
               supabaseClient={supabase}
               appearance={{ theme: ThemeMinimal }}
               theme="default"
