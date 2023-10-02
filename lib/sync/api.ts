1c7aa207dc32b1b89b4059b5620988645a2997d2
Michael Fester <michael.fester@gmail.com>
2023-10-01 20:30:09 -0700

    Cleanup

---
+++ lib/sync/api.ts
@@ -0,0 +1,3 @@
+import { NangoSyncWebhookBody } from '@nangohq/node';
+
+export const syncNangoRecords = async (payload: NangoSyncWebhookBody) => {};
--- pages/api/webhooks/nango/sync.ts
+++ pages/api/webhooks/nango/sync.ts
@@ -1,4 +1,7 @@
+import { NangoSyncWebhookBody } from '@nangohq/node';
 import type { NextApiRequest, NextApiResponse } from 'next';
+
+import { syncNangoRecords } from '@/lib/sync/api';
 
 type Data =
   | {
@@ -13,13 +16,22 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
-  console.log('IN HERE!', req.method);
   if (!req.method || !allowedMethods.includes(req.method)) {
     res.setHeader('Allow', allowedMethods);
     return res.status(405).json({ error: `Method ${req.method} Not Allowed` });
   }
 
-  console.log('BODY', JSON.stringify(req.body, null, 2));
+  // {
+  //   "connectionId": "<user-id>",
+  //   "providerConfigKey": "<integration-id>",
+  //   "syncName": "<sync-name>",
+  //   "model": "<data-model>",
+  //   "responseResults": { "<DataModel>": { "added": 123, "updated": 123, "deleted": 123 } },
+  //   "syncType": "INITIAL" | "INCREMENTAL",
+  //   "queryTimeStamp": "2023-05-31T11:46:13.390Z",
+  // }
+
+  syncNangoRecords(JSON.parse(req.body) as NangoSyncWebhookBody);
 
   return res.status(200).json({});
 }
