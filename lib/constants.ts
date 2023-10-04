fce6f7a277efe3bacd4e8f2df7ae9f17cdb1b40c
Michael Fester <michael.fester@gmail.com>
2023-10-04 08:35:39 -0700

    Update markprompt-js package versions

--- lib/constants.ts
+++ lib/constants.ts
@@ -21,8 +21,8 @@ export const DEFAULT_MARKPROMPT_CONFIG = `{
 }`;
 
 export const MARKPROMPT_JS_PACKAGE_VERSIONS = {
-  css: '0.16.1',
-  web: '0.19.3',
-  react: '0.25.1',
-  'docusaurus-theme-search': '0.12.2',
+  css: '0.16.3',
+  web: '0.19.4',
+  react: '0.26.1',
+  'docusaurus-theme-search': '0.13.3',
 };
