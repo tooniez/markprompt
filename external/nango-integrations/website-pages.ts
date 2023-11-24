a30d6a433fa14b09446c014d092c31a4af7c0815
Michael Fester <michael.fester@gmail.com>
2023-11-23 17:57:55 -0800

    Wrap loggin on syncs

--- external/nango-integrations/website-pages.ts
+++ external/nango-integrations/website-pages.ts
@@ -62,26 +62,28 @@ const appendToLogFull = async (
   const markpromptAPIToken = getEnv(env, 'MARKPROMPT_API_TOKEN');
 
   if (!markpromptUrl || !markpromptAPIToken) {
-    return undefined;
+    return;
   }
 
   await nango.log('appendToLogFull: ' + syncQueueId + ' ' + markpromptUrl);
 
-  const res = await nango.proxy({
-    method: 'POST',
-    baseUrlOverride: markpromptUrl,
-    endpoint: `/api/sync-queues/${syncQueueId}/append-log`,
-    providerConfigKey: PROVIDER_CONFIG_KEY,
-    connectionId: nango.connectionId!,
-    data: { message, level },
-    headers: {
-      Authorization: `Bearer ${markpromptAPIToken}`,
-      'Content-Type': 'application/json',
-      accept: 'application/json',
-    },
-  });
-
-  return res.data;
+  try {
+    await nango.proxy({
+      method: 'POST',
+      baseUrlOverride: markpromptUrl,
+      endpoint: `/api/sync-queues/${syncQueueId}/append-log`,
+      providerConfigKey: PROVIDER_CONFIG_KEY,
+      connectionId: nango.connectionId!,
+      data: { message, level },
+      headers: {
+        Authorization: `Bearer ${markpromptAPIToken}`,
+        'Content-Type': 'application/json',
+        accept: 'application/json',
+      },
+    });
+  } catch (e) {
+    await nango.log(`Error posting log: ${e}`);
+  }
 };
 
 type EnvEntry = { name: string; value: string };
